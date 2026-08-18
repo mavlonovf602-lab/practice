8b04bcc711b97c9336a0ed69c212b34b9a303935
mavlonovf602-lab <mavlonovf602@gmail.com>
2026-08-18 11:50:19 +0900

    feat: set and dic comprehensionlarni ko'rib chiqdim

--- comprehansions.py
+++ comprehansions.py
@@ -16,3 +16,10 @@ cars = [("bmw", 1988),
         ("ford", 2003)]
 old_cars = [car[0] for car in cars if car[1] > 1990]
 print("old_cars:", old_cars)
+numbers = [1, 2, 3, 4, 5, 54, 3, 32, 1, 3, 4, 3, 2]
+set_numbs = {*numbers}
+print("setr_numbers:", set_numbs)
+dic_people = {person[0]: person[1] for person in people}
+print("p---", dic_people)
+dic_people1 = {person[0]: person[1] for person in people if person[1] > 23}
+print("p---", dic_people1)
